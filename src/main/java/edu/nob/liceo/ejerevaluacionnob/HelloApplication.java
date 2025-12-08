fda1487928e51e00536deabc16bf2fb281b49cd6
nico <nicolasoterobouza@gmail.com>
2025-12-08 12:37:15 +0100

    Icono de app

--- src/main/java/edu/nob/liceo/ejerevaluacionnob/HelloApplication.java
+++ src/main/java/edu/nob/liceo/ejerevaluacionnob/HelloApplication.java
@@ -3,7 +3,9 @@ package edu.nob.liceo.ejerevaluacionnob;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+
 
 import java.io.IOException;
 
@@ -12,6 +14,15 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+
+        try {
+
+            Image icon = new Image(getClass().getResourceAsStream("/edu/nob/liceo/ejerevaluacionnob/recursos/icono/palogolfinicio.png"));
+            stage.getIcons().add(icon);
+        } catch (Exception e) {
+            System.err.println("No se pudo cargar el icono. Revisa que la carpeta 'iconos' esté en resources: " + e.getMessage());
+        }
+
         stage.setTitle("Inicio Sesión");
         stage.setScene(scene);
         stage.show();
